bff699d681c8d944bf9666ab01cbc8f1cde63312
Cristian Rojas <credrope@hotmail.com>
2023-08-15 22:58:52 -0500

    Ingesta de campos

---
+++ src/field_ing_full.py
@@ -0,0 +1,51 @@
+import os
+import time
+from elasticsearch_config import es
+from config import *
+from index_constants import fields
+
+txt_file_path = os.getenv("TXT_FILE_PATH")
+index_name = fields
+
+field_lengths = [3, 8, 3, 1, 5, 32]
+field_names = [
+    "field-district",
+    "field-number",
+    "field-county-code",
+    "field-multi-couties",
+    "field-classification",
+    "field-name",
+]
+
+
+def split_line(line, field_lengths, field_names):
+    record = {}
+    start = 0
+    for index, length in enumerate(field_lengths):
+        field_name = field_names[index]
+        field_value = line[start : start + length].strip()
+        record[field_name] = field_value
+        start += length
+    return record
+
+
+def ingestion_fields():
+    with open(txt_file_path, "r", encoding="utf-8") as txt_file:
+        data = []
+        for line_number, line in enumerate(txt_file, start=1):
+            line = line.rstrip("\n")
+            record = split_line(line, field_lengths, field_names)
+            data.append(record)
+
+    for item in data:
+        es.index(index=index_name, document=item)
+
+    return len(data)
+
+
+def main():
+    start_time = time.time()
+    records = ingestion_fields()
+    end_time = time.time()
+    all_time = end_time - start_time
+    print(f"Ingesta fields, Tiempo: {all_time} seg., Cantidad registros : {records}")
